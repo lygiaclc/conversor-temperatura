215576f887c5fd14356fee0c40afa89d2d57c234
Lygia Cipriano <121272073+lygiaclc@users.noreply.github.com>
2023-01-25 19:23:29 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+var temperature = prompt("Qual a temperatura em Celsius?");
+
+// converter a temperatura para farenheit (°F)
+var convertToFarenheit = (temperature*1.8) + 32;
+
+//- mostrar o resultado no console 
+console.log("A temperatura em Farenheit é: " + convertToFarenheit);
